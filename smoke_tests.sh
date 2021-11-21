2b47cb1f535ee6de4d0cc911f32748b288efd9ee
davothebigafro <davoxdkj@gmail.com>
2021-11-21 19:19:27 +0000

    update smoke_tests

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-BODY=$(curl -s http://localhost:80/dashboards)
+BODY=$(curl -s http://localhost:3001/dashboards)
 FAILED_TESTS=0
 
-LENGTH=$(echo "$BODY" | jq length)
-if ! [[ $LENGTH -ge 0 ]]; then
+if [[ -z $BODY ]]; then
   echo "it should return at least one dashboard: FAIL" 1>&2
   ((FAILED_TESTS++))
 else
-  echo "it should return at least one dashboard: PASS"
+  LENGTH=$(echo "$BODY" | jq length)
+  if ! [[ $LENGTH -ge 0 ]]; then
+    echo "it should return at least one dashboard: FAIL" 1>&2
+    ((FAILED_TESTS++))
+  else
+    echo "it should return at least one dashboard: PASS"
+  fi
 fi
 
 ID=$(echo "$BODY" | jq '.[0] | .id')
-if ! [[ $ID -ge 0 ]]; then
+if [[ -z $ID ]]; then
   echo "it should include a numeric id: FAIL" 1>&2
   ((FAILED_TESTS++))
 else
-  echo "it should include a numeric id: PASS"
+  if ! [[ $ID -ge 0 ]]; then
+    echo "it should include a numeric id: FAIL" 1>&2
+    ((FAILED_TESTS++))
+  else
+    echo "it should include a numeric id: PASS"
+  fi
 fi
 
 CREATED_AT=$(echo "$BODY" | jq '.[0] | .createdAt')
-if [[ $CREATED_AT == null ]]; then
+if [[ -z $CREATED_AT ]]; then
   echo "it should include a non empty createdAt: FAIL" 1>&2
   ((FAILED_TESTS++))
 else
@@ -28,7 +38,7 @@ else
 fi
 
 UPDATED_AT=$(echo "$BODY" | jq '.[0] | .updatedAt')
-if [[ $UPDATED_AT == null ]]; then
+if [[ -z $UPDATED_AT ]]; then
   echo "it should include a non empty updatedAt: FAIL" 1>&2
   ((FAILED_TESTS++))
 else
@@ -36,7 +46,7 @@ else
 fi
 
 TITLE=$(echo "$BODY" | jq '.[0] | .title')
-if [[ $TITLE == null ]]; then
+if [[ -z $TITLE ]]; then
   echo "it should include a non empty title: FAIL" 1>&2
   ((FAILED_TESTS++))
 else
