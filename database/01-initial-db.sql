4b66eebdad4aa46c1e747355b7f802c7ef81b1d5
davothebigafro <davoxdkj@gmail.com>
2021-11-23 15:51:59 +0000

    Improve sql start up script

--- database/01-initial-db.sql
+++ database/01-initial-db.sql
@@ -2,7 +2,10 @@ use definitions;
 
 CREATE TABLE IF NOT EXISTS `dashboards` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NOT NULL,
   `createdAt` TIMESTAMP NOT NULL DEFAULT NOW(),
   `updatedAt` TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO dashboards (title) VALUES ('Dashboard A'), ('Dashboard B'), ('Dashboard C'), ('Dashboard D');
