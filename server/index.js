ec13d770c69264af2e1c8071f4d2b20fd33cc168
davothebigafro <davoxdkj@gmail.com>
2021-11-23 15:53:09 +0000

    Server for getting dashboards

--- server/index.js
+++ server/index.js
@@ -1,7 +1,31 @@
 const express = require("express");
-const api = express();
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql2");
 
-const port = 3001;
-api.listen(port, () => {
-  console.log("running on port " + port.toString());
+const connection = mysql.createPool({
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/dashboards", (req, res) => {
+  const sqlSelect = "SELECT * FROM dashboards";
+  connection.query(sqlSelect, (err, result) => {
+    if (err) {
+      console.log("Error occured in getting dashboards from database");
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(process.env.NODE_PORT, () => {
+  console.log("running on port " + process.env.NODE_PORT.toString());
 });
